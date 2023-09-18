e85b42e403497d0135fbf732fd1dd899087ea8b7
Phua Jun Hau <JunHau.Phua@pulsesync.com>
2023-09-18 21:21:03 +0800

    New code and design commit

--- app/src/main/java/com/example/gymbetaapp/MainActivity.kt
+++ app/src/main/java/com/example/gymbetaapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.gymbetaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gymbetaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Navigate to login
+        binding.btnLogin.setOnClickListener {
+            navigateLogin()
+        }
+
+        //Navigate to register
+        binding.btnRegister.setOnClickListener {
+            navigateRegister()
+        }
+    }
+
+    private fun navigateRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateLogin(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
