1b87449609caea973858fcb19765e589832f4301
Phua Jun Hau <JunHau.Phua@pulsesync.com>
2023-09-19 16:00:02 +0800

    New code and design commit v2

---
+++ app/src/main/java/com/example/gymbetaapp/ConfirmForgotPassword.kt
@@ -0,0 +1,11 @@
+package com.example.gymbetaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_forgot_password)
+    }
+}
--- app/src/main/java/com/example/gymbetaapp/RegisterActivity.kt
+++ app/src/main/java/com/example/gymbetaapp/RegisterActivity.kt
@@ -1,11 +1,26 @@
 package com.example.gymbetaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gymbetaapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            navigateConfirm()
+        }
+    }
+
+    private fun navigateConfirm() {
+        intent = Intent(this, ConfirmationActivity::class.java)
+        startActivity(intent)
     }
 }
