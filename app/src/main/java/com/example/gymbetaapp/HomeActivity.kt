275609e1edc3de7c81c6e6b234d08497171fc0fd
Phua Jun Hau <JunHau.Phua@pulsesync.com>
2023-09-21 17:49:03 +0800

    - Text solve - Main Activity with UI - Bottom nav pending

--- app/src/main/java/com/example/gymbetaapp/HomeActivity.kt
+++ app/src/main/java/com/example/gymbetaapp/HomeActivity.kt
@@ -1,11 +1,29 @@
 package com.example.gymbetaapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gymbetaapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvTitle.setShadowLayer(1.6f,1.5f,1.3f,Color.BLACK)
+
+
     }
+
+
 }
