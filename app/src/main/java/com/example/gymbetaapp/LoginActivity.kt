d99d17b3a485132f6c8fbfafc450c14b6544e62d
Phua Jun Hau <JunHau.Phua@pulsesync.com>
2023-09-22 17:51:15 +0800

    - validate access problem (LoginActivity)

--- app/src/main/java/com/example/gymbetaapp/LoginActivity.kt
+++ app/src/main/java/com/example/gymbetaapp/LoginActivity.kt
@@ -1,9 +1,19 @@
 package com.example.gymbetaapp
 
+import android.app.Dialog
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import com.example.gymbetaapp.databinding.ActivityLoginBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,8 +24,31 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            toMainActivity()
+            validateAccess()
+            DialogUtils.showCustomDialog(this, "name")
+//            toMainActivity()
         }
+    }
+
+    private fun validateAccess() {
+        var username = binding.etEmail.text.toString()
+        var password = binding.etPassword.text.toString()
+
+        val db = Firebase.firestore
+
+        db.collection("gym")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    // Implement your access validation logic here
+                    // For example, check if 'username' and 'password' match some data in Firestore
+                    // If validation succeeds, you can navigate to the next screen
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 
     private fun toMainActivity() {
