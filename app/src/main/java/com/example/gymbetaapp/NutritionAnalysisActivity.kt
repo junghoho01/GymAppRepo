a0e6dad3724c7d23a3d41d0c7a56db973aa88e2e
Phua Jun Hau <JunHau.Phua@pulsesync.com>
2023-10-04 13:15:43 +0800

    - Retreive Calorie is working

--- app/src/main/java/com/example/gymbetaapp/NutritionAnalysisActivity.kt
+++ app/src/main/java/com/example/gymbetaapp/NutritionAnalysisActivity.kt
@@ -2,7 +2,10 @@ package com.example.gymbetaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.gymbetaapp.databinding.ActivityNutritionAnalysisBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
 class NutritionAnalysisActivity : AppCompatActivity() {
 
@@ -20,19 +23,39 @@ class NutritionAnalysisActivity : AppCompatActivity() {
     }
 
     private fun analyseFood(ingredients: String) {
-        val ingredientsFood = ingredients.split(", ")
-
-        // Add a newline character after each ingredient
-        val formattedIngredients = ingredientsFood.joinToString(",\n")
+//        // Split the ingredients by ", " and trim whitespace
+//        val ingredientList = ingredients.split(",\n").map { it.trim() }
+//
+//        // Create a JSON array of ingredients
+//        val jsonArray = JSONArray()
+//        for (ingredient in ingredientList) {
+//            jsonArray.put(ingredient)
+//        }
+//
+//        // Log the JSON data before making the API request
+//        Log.d("API Request", jsonArray.toString())
 
         val edamamApiClient = EdamamApiClient()
-        edamamApiClient.fetchNutritionData(formattedIngredients, object : NutritionCallback {
+        edamamApiClient.fetchNutritionData(ingredients, object : NutritionCallback {
             override fun onSuccess(response: NutritionResponse?) {
                 // Handle the successful response here
                 if (response != null) {
                     println("Calories: ${response.calories}")
                     println("Total Nutrients: ${response.totalNutrients}")
                     println("Total Daily: ${response.totalDaily}")
+//                    binding.tvCalories.text = response.calories.toString()
+
+                    // Retrieve total FAT
+                    val totalFat = response.totalNutrients["FAT"]
+                    if (totalFat != null) {
+                        val fatLabel = totalFat.label
+                        val fatQuantity = totalFat.quantity
+                        val fatUnit = totalFat.unit
+                        println("Total Fat: $fatQuantity $fatUnit ($fatLabel)")
+
+                        // Now, you can set the total fat to your TextView
+                        binding.tvCalories.text = "$fatQuantity $fatUnit"
+                    }
                 }
             }
 
